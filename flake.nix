ebcdc68cd0b45bd6c5a20a28dfa5b878bb4f3632
Nate <ezlo@ezlo.manatee-duck.ts.net>
2025-01-26 19:01:10 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+
+      nixpkgs.config.allowUnfree = true;
+
+      environment.systemPackages = [ 
+	      pkgs.alacritty
+        ];
+
+      fonts.packages = [
+	      pkgs.nerd-fonts.jetbrains-mono
+        ];
+
+      homebrew = {
+          enable = true;
+          brews = [
+            "mas"
+          ];
+          casks = [
+            "iina"
+          ];
+          masApps = {
+              "CotEditor" = 1024640650;
+          };
+          onActivation.cleanup = "zap";
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+	  programs.zsh.enable = true;
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    darwinConfigurations."Ezlo" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        mac-app-util.darwinModules.default
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "ezlo";
+          };
+        }
+       ];
+    };
+  };
+}
