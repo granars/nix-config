48b4090893e866cff2d90a120f7091b2460beaef
granars <github@granars.cloud>
2025-06-15 22:24:34 +0100

    1password changes / ssh config + cleanup

--- users/granar/gitconfig.nix
+++ users/granar/gitconfig.nix
@@ -16,7 +16,7 @@
       };
 
       user = {
-        signingKey = "...";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3FK9J6CrT8NPGx11kUigv7vSaSDikC/jo4nA+P8gWl";
       };
     };
   };
