7bdae0ed51d154ea9005059fa656c18167ebd2a7
granars <github@granars.cloud>
2025-07-03 19:57:20 +0100

    git changes

--- machines/nixos/_commonhome/default.nix
+++ machines/nixos/_commonhome/default.nix
@@ -4,16 +4,18 @@
     enable = true;
     userName = "nate";
     userEmail = "github@granars.cloud";
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      s = "status";
+    };
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3FK9J6CrT8NPGx11kUigv7vSaSDikC/jo4nA+P8gWl";
+      signByDefault = true;
+    };
     extraConfig = {
       gpg = {
         format = "ssh";
-      };
-      commit = {
-        gpgsign = true;
-      };
-
-      user = {
-        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3FK9J6CrT8NPGx11kUigv7vSaSDikC/jo4nA+P8gWl";
       };
     };
   };
