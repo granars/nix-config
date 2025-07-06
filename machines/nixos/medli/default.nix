fe16a4de34d7ab4aed74149428e046b10cdac1a1
nate <github@granars.cloud>
2025-07-06 18:02:35 +0100

    added kde connect

--- machines/nixos/medli/default.nix
+++ machines/nixos/medli/default.nix
@@ -109,6 +109,9 @@ in
   # Enable the KDE Plasma Desktop Environment
   services.desktopManager.plasma6.enable = true;
 
+  # Enable KDE Connect
+  programs.kdeconnect.enable = true;
+
   # Excluding some KDE applications from the default install
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     ark
